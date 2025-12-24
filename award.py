e873fb701bb4743fe488e279fbaafb5a46479ef3
YOUSRASIRAJ <yousra.siraj2001@gmail.com>
2025-12-23 23:26:11 -0600

    Add files via upload

---
+++ award.py
@@ -0,0 +1,19 @@
+# Triathlon Award Program
+Swimming = int(input("Enter your Swimming time in (minutes): "))
+Cycling = int(input("Enter your Cycling time in (minutes): "))    
+Running = int(input("Enter your Running time in (minutes): "))
+
+Total_time = Swimming + Cycling + Running
+
+print("Total Time in minutes in Triathlon Award: ", Total_time, "minutes")
+
+if Total_time <= 100:
+    award = "Provincial Colours"
+elif Total_time <= 105:
+    award = "Provincial Half Colours"
+elif Total_time <= 110:
+    award = "Provincial Scroll"
+else:
+    award = "No award"
+print("Award:", award)
+
